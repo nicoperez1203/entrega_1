e97293535e765d19b582927d08451d029e7ba307
Nicolas Perez <nipefo1203@gmail.com>
2021-09-03 08:24:03 -0300

    Entrega2CasiFin

--- js/index.js
+++ js/index.js
@@ -3,4 +3,17 @@ function guardarNomb(){
     sessionStorage.setItem("user", JSON.stringify({usuario: inputUser.value}));
 }
 
+function redireccion(){
+    
+    let user2 = document.getElementById("usuario").value;
+    let pass2 = document.getElementById("pass").value;
+    
+    if (user2 != "" && pass2 != "" ){ 
+            guardarNomb()       
+            window.location = "home.html";
+        }else{
+            alert("Los campos usuario y/o contraseña, no pueden estar vacíos.")
+        }
+    }
+
 
